0449e00f67111970b47eb06e2adbd35cd91a2bb8
Russell Wong <russwong89@gmail.com>
2021-05-22 20:42:42 -0400

    Add random-teams.ts to devolution mod

---
+++ data/mods/devolution/random-teams.ts
@@ -0,0 +1,8 @@
+import type {PRNG} from '../../../sim';
+import RandomTeams, {MoveCounter} from '../../random-teams';
+
+export class RandomDevolutionTeams extends RandomTeams {
+
+}
+
+export default RandomDevolutionTeams;
